76449850615a7250ca6a7a41b8eed2770815ad9f
JanakeesshHD <160233936+JanakeesshHD@users.noreply.github.com>
2024-03-13 12:06:53 +0530

    Add files via upload

---
+++ EventManagement/app.js
@@ -0,0 +1,87 @@
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+let themeToggler = document.querySelector(".theme-toggler");
+let toggleBtn = document.querySelector(".toggle-btn");
+
+toggleBtn.onclick = () => {
+  themeToggler.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  themeToggler.classList.remove("active");
+};
+
+document.querySelectorAll(".theme-toggler .theme-btn").forEach((btn) => {
+  btn.onclick = () => {
+    let color = btn.style.background;
+    document.querySelector(":root").style.setProperty("--theme-color", color);
+  };
+});
+
+var swiper = new Swiper(".home-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".review-slider", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1050: {
+      slidesPerView: 3,
+    },
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+//typing animation script
+var typed = new Typed('#element', {
+  strings: ['Kanasu Events...'],
+  typeSpeed: 50,
+});
+
+//customer review see more see less button
+function toggleReadMore(id) {
+  var content = document.getElementById(id);
+  var button = event.target;
+  if (content.style.display === "none") {
+    content.style.display = "block";
+    button.textContent = "Read Less";
+  } else {
+    content.style.display = "none";
+    button.textContent = "Read More";
+  }
+}
